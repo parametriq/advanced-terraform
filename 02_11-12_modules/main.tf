961a3687c1319383161782e38c40e58eef75374f
Andrey Trubnikov <parametriq@gmail.com>
2022-02-19 22:50:30 +0300

    VPC setup with a module (3 azs)

--- 02_11-12_modules/main.tf
+++ 02_11-12_modules/main.tf
@@ -9,14 +9,8 @@ variable "region" {
   default = "us-east-2"
 }
 
-
-# //////////////////////////////
-# PROVIDERS
-# //////////////////////////////
-provider "aws" {
-  access_key = var.aws_access_key
-  secret_key = var.aws_secret_key
-  region     = var.region
+variable "endpoint" {
+  default = "http://localhost:4566"
 }
 
 # //////////////////////////////
@@ -27,30 +21,30 @@ resource "aws_security_group" "sg_frontend" {
   vpc_id = module.vpc.vpc_id
 
   ingress {
-    from_port = 80
-    to_port = 80
-    protocol = "tcp"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 443
-    to_port = 443
-    protocol = "tcp"
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   ingress {
-    from_port = 22
-    to_port = 22
-    protocol = "tcp"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
   egress {
-    from_port = 0
-    to_port = 0
-    protocol = "-1"
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
@@ -61,8 +55,8 @@ resource "aws_security_group" "sg_frontend" {
 # //////////////////////////////
 module "vpc" {
   source = "terraform-aws-modules/vpc/aws"
-  name = "frontend-vpc"
-  cidr = "10.0.0.0/16"
+  name   = "frontend-vpc"
+  cidr   = "10.0.0.0/16"
 
   azs             = ["us-east-2a", "us-east-2b", "us-east-2c"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
